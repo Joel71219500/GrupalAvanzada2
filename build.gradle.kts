f9a6a571aad9755d3a261c4ffb67b2d387815e8c
Aaron7121 <jaguamangallo@uce.edu.ec>
2025-07-09 23:06:40 -0500

    Estructura general del proyecto Cosas por hacer: -averiguar como integrar con esceneBuilder -conexion a base de datos -logica e implementacion con spring Framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.avanzada2.practicaCDI"
@@ -12,8 +13,25 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // SPRING
+    implementation ("org.springframework:spring-context:6.1.5")
+    implementation ("org.springframework.data:spring-data-jdbc:3.2.5")
+
+    // POSTGRESQL
+    runtimeOnly ("org.postgresql:postgresql:42.7.3")
+
+    // JAVAFX
+    implementation ("org.openjfx:javafx-controls:21")
+    implementation ("org.openjfx:javafx-fxml:21")
+
 }
+
+application {
+    mainClass.set("com.tuempresa.springfxapp.MainApp")
+}
+
 
 tasks.test {
     useJUnitPlatform()
 }
+
---
+++ src/main/java/com/avanzada/grupal/Main.java
@@ -0,0 +1,7 @@
+package com.avanzada.grupal;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
